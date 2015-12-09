c9b036c1c90c7be6fc36018336f54e288f172868
Jeff DeButte <jeff@inharmonic.com>
2015-12-08 19:30:27 -0500

    Add drawing indicator lines.

--- src/app/components/harmonograph/harmonograph.directive.js
+++ src/app/components/harmonograph/harmonograph.directive.js
@@ -18,8 +18,9 @@
       bindToController: true,
       link: function(scope, element, attrs) {
       	var d3 = $window.d3;
-      	var amplitude = 200;
+      	var amplitude = 400;
       	var margin = 10;
+      	var r = 4;
 
       	var svg = d3.select('.svg-container')
       		.append('svg')
@@ -27,48 +28,73 @@
       		.attr('height', 400);
 
       	var xOsc = svg
-      		.append('circle')
+      		.append("g")
       		.attr('class', 'x-osc')
-      		.attr('freq', scope.settings.x.freq)
-      		.attr('r', 4)
-      		.attr('cx', margin)
-      		.attr('cy', 200)
-      		.transition()
-      		.duration(scope.settings.duration/scope.settings.x.freq)
-      		.each(slideX);
+  
+
+      	xOsc.append('circle')
+      		.attr('data-freq', scope.settings.x.freq)
+      		.attr('data-pen', 'x')
+      		.attr('r', r)
+      		.attr('cx', r)
+      		.attr('cy', r)
+
+      	xOsc.append('line')
+      		.attr('x1', r)
+      		.attr('x2', amplitude - r)
+      		.attr('y1', r)
+      		.attr('y2', r)
+      		.style('stroke-width', '1px')
+      		.style('stroke', '#eee');
 
       	var yOsc = svg
-      		.append('circle')
+      		.append("g")
       		.attr('class', 'y-osc')
-      		.attr('freq', scope.settings.y.freq)
-      		.attr('r', 4)
-      		.attr('cx', 200)
-      		.attr('cy', margin)
-      		.transition()
+  
+
+      	yOsc.append('circle')
+      		.attr('data-freq', scope.settings.x.freq)
+      		.attr('data-pen', 'y')
+      		.attr('r', r)
+      		.attr('cx', amplitude)
+      		.attr('cy', r)
+
+      	yOsc.append('line')
+      		.attr('x1', amplitude)
+      		.attr('x2', amplitude)
+      		.attr('y1', r)
+      		.attr('y2', amplitude)
+      		.style('stroke-width', '1px')
+      		.style('stroke', '#eee');
+
+      	xOsc.transition()
+      		.ease('linear')
+      		.duration(scope.settings.duration/scope.settings.x.freq)
+      		.each(slideX);
+      	yOsc.transition()
+      		.ease('linear')
       		.duration(scope.settings.duration/scope.settings.y.freq)
       		.each(slideY);
 
 
  		function slideX() {
- 		  var circle = d3.select(this);
+ 		  var osc = d3.select(this);
  		  (function repeat() {
- 		    circle = circle.transition()
- 		        .attr("cy", 200 - amplitude + margin)
+ 		    osc = osc.transition()
+ 		        .attr("transform", "translate(0, 0)")
  		      .transition()
- 		        .attr("cy", 200)
+ 		        .attr("transform", "translate(0, "+(amplitude - r)+")")
  		        .each("end", repeat);
  		  })();
  		}
 
  		function slideY() {
- 		  var circle = d3.select(this);
+ 		  var osc = d3.select(this);
  		  (function repeat() {
- 		    circle = circle.transition()
- 		        .attr("cx", function() {
- 		        	return 200 - amplitude + margin;
- 		        })
+ 		    osc = osc.transition()
+ 		        .attr("transform", "translate("+(r - amplitude) +", 0)")
  		      .transition()
- 		        .attr("cx", 200)
+ 		        .attr("transform", "translate(0, 0)")
  		        .each("end", repeat);
  		  })();
  		}
